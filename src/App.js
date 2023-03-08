e98bec73af6200606684cb315a3d129e39536012
tbrown034 <tbrown034@gmail.com>
2023-03-07 18:35:21 -0600

    hello template

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Header, Footer, HomeHero } from './Components';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <HomeHero />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
